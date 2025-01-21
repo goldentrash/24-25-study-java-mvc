4e384f08cc3f4b0fc7d026cdd54df56419a41991
WhitePiano <startoflateness@gmail.com>
2025-01-21 17:00:04 +0900

    feat(mvc): implement JsonView

--- mvc/src/main/java/com/interface21/web/view/JsonView.java
+++ mvc/src/main/java/com/interface21/web/view/JsonView.java
@@ -1,13 +1,19 @@
 package com.interface21.web.view;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.interface21.web.http.MediaType;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import java.util.Map;
 
 public class JsonView implements View {
 
+    private final ObjectMapper objectMapper = new ObjectMapper();
+
     @Override
     public void render(final Map<String, ?> model, final HttpServletRequest request, HttpServletResponse response)
             throws Exception {
+        response.setContentType(MediaType.APPLICATION_JSON_UTF8_VALUE);
+        objectMapper.writeValue(response.getWriter(), model);
     }
 }
